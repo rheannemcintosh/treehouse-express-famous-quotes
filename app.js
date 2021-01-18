ad1335139ba52044ce64b92fc1413f1c139a4482
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-01-18 23:42:01 +0000

    Send a GET request to /quotes to READ a list of quotes

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const express = require('express');
 const app = express();
 
 // Send a GET request to /quotes to READ (view) a list of quotes
+app.get('/quotes', (req, res) => {
+    res.json(data);
+});
+
 // Send a GET request to /quotes/:id to READ(view) a quote
 // Send a POST request to /quotes to CREATE a new quote
 // Send a PUT request to /quotes/:id to UPDATE (edit) a quote
