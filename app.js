500a377fae2ee8a4076035555dc2f12cbaa85187
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-01-18 23:41:31 +0000

    Add comments to split up required tasks

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const express = require('express');
 //Create an Express Application
 const app = express();
 
+// Send a GET request to /quotes to READ (view) a list of quotes
+// Send a GET request to /quotes/:id to READ(view) a quote
+// Send a POST request to /quotes to CREATE a new quote
+// Send a PUT request to /quotes/:id to UPDATE (edit) a quote
+// Send a DELETE request to /quotes/:id to DELETE a quote
+// Send a GET request to /quotes/quote/random to READ (view) a random quote
+
 //Listen on port 3000
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
 
