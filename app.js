1444d7a54c8186fed6888ba96213f9d47e0ff965
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-01-19 19:57:41 +0000

    Refactor the two GET routes to use separate records.js file

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ const express = require('express');
 //Create an Express Application
 const app = express();
 
+// Input records.js
+const records = require('./records');
+
 // Send a GET request to /quotes to READ (view) a list of quotes
-app.get('/quotes', (req, res) => {
-    res.json(data);
+app.get('/quotes', async (req, res) => {
+    const quotes = await records.getQuotes();
+    res.json(quotes);
 });
 
 // Send a GET request to /quotes/:id to READ(view) a quote
-app.get('/quotes/:id', (req, res) => {
-    const quote = data.quotes.find(quote => quote.id == req.params.id);
+app.get('/quotes/:id', async (req, res) => {
+    const quote = await records.getQuote(req.params.id);
     res.json(quote);
 });
 
@@ -22,23 +26,3 @@ app.get('/quotes/:id', (req, res) => {
 
 //Listen on port 3000
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
-
-const data = {
-    quotes: [
-        {
-            id: 8721,
-            quote: "We must accept finite disappointment, but we must never lose infinite hope.",
-            author: "Martin Luther King"
-        },
-        {
-            id: 5779,
-            quote: "Use what you’ve been through as fuel, believe in yourself and be unstoppable!",
-            author: "Yvonne Pierre"
-        },
-        {
-            id: 3406,
-            quote: "To succeed, you have to do something and be very bad at it for a while. You have to look bad before you can look really good.",
-            author: "Barbara DeAngelis"
-        }
-    ]
-}
